5a16fce54c062b02b1f7426e412ffa69a0807a25
Khurshedjon Rajabzoda <122232005+rajabzoda2810@users.noreply.github.com>
2026-07-22 17:51:27 +0500

    Update userName state initialization logic

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ const App: React.FC = () => {
   const profitPerHour = 1;
 const [userName, setUserName] = React.useState(() => {
     const tgUser = (window as any).Telegram?.WebApp?.initDataUnsafe?.user;
-    return tgUser ? ${tgUser.first_name} (Builder) : "Nikandr (CEO)";
+   return tgUser?.first_name ? ${tgUser.first_name} (Builder) : "НЕТ ДАННЫХ ИЗ ТГ";
   });
   const [dailyRewardTimeLeft, setDailyRewardTimeLeft] = useState("");
   const [dailyCipherTimeLeft, setDailyCipherTimeLeft] = useState("");
